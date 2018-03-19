ecf155e5bf257ab9638e2238859bd7a59fdb8a9c
Sean Connolly <seanconnolly@mail.rit.edu>
2018-03-18 22:08:33 -0400

    Did instance creation.
    Memory access violation happening in vkCreateInstance() currently though.

--- LearningVulkan/LearningVulkan/TriApp.cpp
+++ LearningVulkan/LearningVulkan/TriApp.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 class TriApp
 {
@@ -21,6 +22,7 @@ public:
 
 private:
 	GLFWwindow* window;
+	VkInstance instance;
 
 	void initWindow()
 	{
@@ -34,9 +36,40 @@ private:
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	}
 
+	void createInstance()
+	{
+		VkApplicationInfo appInfo = {};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_1;
+
+
+		VkInstanceCreateInfo createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		createInfo.flags = 0;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+		
+		//crashes on create instance
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create instance!");
+		}
+	}
+
 	void initVulkan()
 	{
-		//s
+		createInstance();
 	}
 
 	void mainLoop()
