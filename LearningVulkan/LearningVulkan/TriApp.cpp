7fcece66ec198eb58013201ad0d950a0de117883
Sean Connolly <seanconnolly@mail.rit.edu>
2018-03-30 20:48:11 -0400

    Fixed Crashing. Added EXT checking.
    Crashing was due to outdated AMD graphics drivers. I updated them and that
    fixed it. Also, I figured out the required extensions for GLFW and
    compared them against available extensions. If we do not have all
    available extensions, then we throw an error.

--- LearningVulkan/LearningVulkan/TriApp.cpp
+++ LearningVulkan/LearningVulkan/TriApp.cpp
@@ -1,3 +1,5 @@
+#include <Windows.h>
+
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 
@@ -46,7 +48,6 @@ private:
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.apiVersion = VK_API_VERSION_1_1;
 
-
 		VkInstanceCreateInfo createInfo = {};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		createInfo.pApplicationInfo = &appInfo;
@@ -54,17 +55,59 @@ private:
 
 		uint32_t glfwExtensionCount = 0;
 		const char** glfwExtensions;
-
 		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		//check required extensions against available extensions
+		if (!checkEXT(glfwExtensions, glfwExtensionCount))
+		{
+			throw std::runtime_error("not all required extensions supported!");
+		}
+
 		createInfo.enabledExtensionCount = glfwExtensionCount;
 		createInfo.ppEnabledExtensionNames = glfwExtensions;
 		createInfo.enabledLayerCount = 0;
 		
-		//crashes on create instance
+		//fixed, my vulkan drivers were too old
 		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
 		{
 			throw std::runtime_error("failed to create instance!");
 		}
+	}
+
+	bool checkEXT(const char** reqEXT, uint32_t reqCount)
+	{
+		uint32_t extCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extCount);
+
+		vkEnumerateInstanceExtensionProperties(nullptr, &extCount, extensions.data());
+
+		std::cout << "available extensions: " << std::endl;
+		for (const auto& extension : extensions)
+		{
+			std::cout << "\t" << extension.extensionName << std::endl;
+		}
+
+		std::cout << "required extensions: " << std::endl;
+		for (uint32_t i = 0; i < reqCount; i++)
+		{
+			std::cout << "\t" << reqEXT[i] << std::endl;
+		}
+
+		uint32_t count = 0;
+		for (uint32_t i = 0; i < reqCount; i++)
+		{
+			for (uint32_t j = 0; j < extensions.size(); j++)
+			{
+				if (strcmp(reqEXT[i], extensions[j].extensionName) == 0)
+					count++;
+			}
+		}
+
+		if(count == reqCount)
+			return true;
+
+		return false;
 	}
 
 	void initVulkan()
@@ -82,6 +125,8 @@ private:
 
 	void cleanup()
 	{
+		vkDestroyInstance(instance, nullptr);
+
 		glfwDestroyWindow(window);
 
 		glfwTerminate();
