d2bc4ebb7f69631380f3fe8a433ee0a7bea8d8e2
Sean Connolly <seanconnolly@mail.rit.edu>
2018-04-16 23:56:41 -0400

    Create texture image.

--- LearningVulkan/LearningVulkan/TriApp.cpp
+++ LearningVulkan/LearningVulkan/TriApp.cpp
@@ -110,6 +110,8 @@ private:
 	VkDeviceMemory uniformBufferMemory;
 	VkDescriptorPool descriptorPool;
 	VkDescriptorSet descriptorSet;
+	VkImage textureImage;
+	VkDeviceMemory textureImageMemory;
 
 	const std::vector<Vertex> vertices = {
 		{ { -0.5f, -0.5f },{ 1.0f, 0.0f, 0.0f } },
@@ -1806,7 +1808,7 @@ private:
 
 		if (!pixels)
 		{
-			throw std::runtime_error("failed to load texture image");
+			THROW("failed to load texture image")
 		}
 
 		VkBuffer stagingBuffer;
@@ -1822,6 +1824,60 @@ private:
 		vkUnmapMemory(device, stagingBufferMemory);
 
 		stbi_image_free(pixels);
+
+		createImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_TILING_OPTIMAL,
+			VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory);
+	}
+
+	void createImage(uint32_t width, uint32_t height, VkFormat format,
+		VkImageTiling tiling, VkImageUsageFlags usage,
+		VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory)
+	{
+		VkImageCreateInfo imageInfo = {};
+		imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+		imageInfo.imageType = VK_IMAGE_TYPE_2D;
+		imageInfo.extent.width = width;
+		imageInfo.extent.height = height;
+		imageInfo.extent.depth = 1;
+		imageInfo.mipLevels = 1;
+		imageInfo.arrayLayers = 1;
+		//should use same format for the texels as the pixels in the buffer
+		//otherwise the copy operation will fail
+		imageInfo.format = format;
+		//tiling mode can't be changed at a later time
+		//since we are using a staging buffer, we can't use tiling_linear
+		imageInfo.tiling = tiling;
+		//images can be one of two possible values:
+		//layout_undefined - not usable by GPU and first transition will discard texels
+		//layout_preinitialized - not usable by GPU and first transition will preserve texels
+		imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		imageInfo.usage = usage;
+		//will only be used by one queue family
+		imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		//only used for images utilized as attachments
+		imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+		imageInfo.flags = 0;	//Optional
+
+		if (vkCreateImage(device, &imageInfo, nullptr, &image) != VK_SUCCESS)
+		{
+			THROW("failed to create image!")
+		}
+
+		VkMemoryRequirements memRequirements;
+		vkGetImageMemoryRequirements(device, image, &memRequirements);
+
+		VkMemoryAllocateInfo allocInfo = {};
+		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+		allocInfo.allocationSize = memRequirements.size;
+		allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, properties);
+
+		if (vkAllocateMemory(device, &allocInfo, nullptr, &imageMemory) != VK_SUCCESS)
+		{
+			THROW("failed to allocate image memory!")
+		}
+
+		vkBindImageMemory(device, image, imageMemory, 0);
 	}
 
 #pragma endregion
